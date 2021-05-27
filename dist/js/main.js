3908cfa44b450e2999dee0fd42bafadfd5ec0079
sammy-ai <matabanesam7@gmail.com>
2021-05-27 14:16:26 +0200

    Added menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,11 @@
-//Select Document Opening Menu(DOM) items
+// Select DOM Items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
-//Set Initial State of Menu
+// Set Initial State of Menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
@@ -16,7 +16,7 @@ function toggleMenu() {
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
-        menuItems.forEach(item => item.classList.add('show'));
+        navItems.forEach(item => item.classList.add('show'));
 
         //Set Menu State
         showMenu = true;
@@ -25,7 +25,7 @@ function toggleMenu() {
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
-        menuItems.forEach(item => item.classList.remove('show'));
+        navItems.forEach(item => item.classList.remove('show'));
 
         //Set Menu State
         showMenu = false;
